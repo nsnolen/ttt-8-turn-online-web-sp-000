296cf03e8de8871ba463deb3eb830e08172d3bc0
Sara Nolen <nolensara1@gmail.com>
2020-11-13 19:00:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ def valid_move?(board, index)
   index = user_input.to_i - 1
  if index.between(0, 8) && board[index] == " " || board[index]== "" || baord[index] == nil
    return true
- elseif index.between(o, 8) && board[index] == "X" || board[index] == "O"
+ elsif index.between(o, 8) && board[index] == "X" || board[index] == "O"
    return false
 end
 end
