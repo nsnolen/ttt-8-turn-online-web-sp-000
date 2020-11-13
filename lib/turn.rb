a196ed3d9896f1dc17c4a13e72e74b2f8ad56b09
Sara Nolen <nolensara1@gmail.com>
2020-11-13 20:40:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,7 +46,7 @@ def turn(board) do
   puts "Please enter 1-9:"
   user_input = gets.strip.to_i - 1
   input_to_index(user_input)
-  if valid_move?(board, user_input) == true
+  if valid_move?(board, user_input)
     break
   end
 end
