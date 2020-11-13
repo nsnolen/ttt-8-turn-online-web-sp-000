b4b8626e7f6a3cdebf8ed2033d0eb71e422a8610
Sara Nolen <nolensara1@gmail.com>
2020-11-13 17:32:38 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+#Ask the user for their move by position 1-9
+display_board
+# Receive the user input
+
+#Convert position to an index
+#Validmove?
+  #make move and display_board
+#inValidmove?
+  #ask for a new move until a valid move is received. 
