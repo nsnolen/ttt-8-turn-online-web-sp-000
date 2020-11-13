be1f29ae9b9090eb6f3e30eb98b75f4b07e5b844
Sara Nolen <nolensara1@gmail.com>
2020-11-13 21:36:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -48,8 +48,6 @@ def turn(board)
   input_to_index(user_input)
   until valid_move?(board, user_input) == true
   return turn(board)
-else
-  true
 end
   move(board, user_input, value = "X")
   display_board(board)
