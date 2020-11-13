95ce43df9eacafcb795aad3fd7ace9f2e6b71e66
Sara Nolen <nolensara1@gmail.com>
2020-11-13 20:46:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,9 +46,9 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip.to_i - 1
   input_to_index(user_input)
-
   until valid_move?(board, user_input) == true
     puts "Please enter 1-9:"
+    break
   end
 
 end
