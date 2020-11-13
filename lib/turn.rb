26e8d2836f80a028c15a3af704953bc0cf67d902
Sara Nolen <nolensara1@gmail.com>
2020-11-13 17:47:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 #display_board
 def display_board(board)
-board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," ","X"," "," "," "," "," "," "]
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -8,8 +8,7 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 #prints arbitrary board
-board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
-display_board(board)
+
 #Ask the user for their move by position 1-9
 
 # Receive the user input
