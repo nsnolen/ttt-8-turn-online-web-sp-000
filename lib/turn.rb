59cba2be632be77edc5f88d00a984751fddb7f10
Sara Nolen <nolensara1@gmail.com>
2020-11-13 17:50:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 #display_board
 def display_board(board)
-board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
