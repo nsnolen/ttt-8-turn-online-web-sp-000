0aab21ffff4ee2b3fb3f972bed19071b468d03e4
Sara Nolen <nolensara1@gmail.com>
2020-11-13 19:05:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,15 +17,29 @@ def input_to_index(user_input)
 end
 
 #Validmove?
+
+
+
 def valid_move?(board, index)
 
- if board[index].between?(0, 8) && board[index] == " " || board[index]== "" || baord[index] == nil
-   return true
- elsif board[index].between?(0, 8) && board[index] == "X" || board[index] == "O"
-   return false
+
+  if index.between?(0,8) && !position_taken?(board, index)
+    return true
+  elsif index.between?(0,8) && position_taken?(board, index)
+    return false
 end
 end
 
+def position_taken?(board, index)
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+
+elsif board[index] == "X" || board[index] == "O"
+  return true
+else
+  return false
+end
+end
   #make move and display_board
 #inValidmove?
   #ask for a new move until a valid move is received.
